2302fa98c3728b175ffb0f3e2fe8ca7618e023f6
kalomuse <jxz405101020@163.com>
2018-06-08 10:12:46 +0800

    init

---
+++ A6073978050129/html/index/index.js
@@ -0,0 +1,130 @@
+var vue = new Vue({
+  el: '.container',
+  data: {
+    website: website,
+    userInfo: {},
+    hasUserInfo: false,
+    bannerlist:{},
+    houselist:[1,2,3],
+    regionid: 330421,
+    region:'嘉善县',
+    purpose:'填写购房意向，让经济人来帮你找房',
+    purposeid:0
+  }
+})
+
+//搜索事件处理函数
+function bindViewsearch() {
+  glo.open_win('../search/search.html');
+}
+//定位事件处理函数
+function bindViewhouse_location() {
+  glo.open_win('../house_location/house_location.html');
+}
+//经济人事件处理函数
+function bindViewsale() {
+  glo.open_win('../salelist/salelist.html');
+}
+//资讯事件处理函数
+function bindViewnews() {
+  glo.open_frame('../news/news.html');
+}
+//房价事件处理函数
+function bindViewprice() {
+  glo.open_win('../real_search/real_search.html');
+}
+//楼盘详情事件处理函数
+function bindViewproview(id) {
+  glo.open_win('../proview/proview.html', {id: id});
+}
+
+function bindViewhouse() {
+  glo.open_win('../prolist/prolist.html');
+}
+
+function bindViewintention() {
+  if(vue.purposeid){
+    glo.open_win('../intention_result/intention_result');
+  }else{
+    glo.open_win('../intention/intention.html');
+  }
+}
+
+apiready = function() {
+  //====================================
+  //首次登录APP后，设置无需再显示登录欢迎页
+  //====================================
+  var key = 'cur_appversion';
+  $api.setStorage(key, api.appVersion);
+  //====================================
+  //回退按钮监听
+  //====================================
+  api.addEventListener({
+      name : 'keyback'
+  }, function(ret, err) {
+      api.confirm({
+          title : '提示',
+          msg : '确认要退出吗?',
+          buttons : ['确定', '取消']
+      }, function(ret, err) {
+          if (ret.buttonIndex == 1) {
+              api.closeWidget({
+                  silent : true
+              });
+          }
+      });
+  });
+
+  if ($api.getStorage('regionid')){
+    vue.regionid =  $api.getStorage('regionid');
+  }
+  $api.setStorage('regionid', vue.regionid);
+
+  //获取地理信息
+  glo.post(website + '/api/region/setRegion', { regionid: vue.regionid }, function (res) {
+      vue.region =  res.data.area;
+  });
+
+ //获取推荐楼盘
+  glo.post(website + '/api/index/getRemhouses', { regionid: vue.regionid }, function (res) {
+      vue.houselist =  res.data;
+  });
+
+  //获取购房意向
+  glo.post(website + '/api/purpose/getPurpose',
+  {
+    uid: $api.getStorage('userid'),
+    areaid: $api.getStorage('regionid')
+  }, function (res) {
+    if(res.data.Code){
+      vue.purpose = res.data.title;
+      vue.purposeid = res.data.id;
+    };
+
+  });
+  //获取banner图
+  var data = {regionid: $api.getStorage('regionid')};
+  glo.post(website + '/api/index/getBanner', data, function(res) {
+    $('.banner').css('height', api.winWidth * 0.433);
+    var img = [];
+    for(var index in res.data) {
+        img.push(website + res.data[index].img);
+    }
+    var UIScrollPicture = api.require('UIScrollPicture');
+    UIScrollPicture.open({
+        rect: {
+            x: 0,
+            y: 40,
+            w: api.winWidth,
+            h: api.winWidth * 0.433
+        },
+        data: {
+            paths: img,
+        },
+        contentMode: 'scaleToFill',
+        fixedOn: api.frameName,
+        fixed: false,
+        interval: 1000
+    });
+  });
+};
---
+++ A6073978050129/script/global.js
@@ -0,0 +1,210 @@
+var website = "http://192.168.18.118:4015";
+//var website = "http://192.168.18.107:5270";
+
+var glo = {
+  init: function(data) {
+    header_h = parseInt($api.getStorage('header_h'));
+		footer_h = parseInt($api.getStorage('footer_h'));
+		main_h = parseInt($api.getStorage('main_h'));
+    token = $api.getStorage('token');
+    if(!data)
+      data = {}
+  },
+  fix_header: function(name) {
+    var header = $api.dom(name);
+    $api.fixIos7Bar(header);
+    api.setStatusBarStyle({
+        style: 'dark',
+        color: '#6ab494'
+    });
+  },
+  open_frame: function(url, pageParams = {}){
+      var arr = url.split('/');
+      var name = arr[arr.length - 1].split('.')[0];
+      if(!arr[arr.length - 2]) {
+        alert('url不规范');
+        return;
+      } else if(arr[arr.length - 2] && arr[arr.length - 2] == '.') {
+        var href = location.href.split('/');
+        name = href[href.length - 2] + '_' + name;
+      } else {
+        name = arr[arr.length - 2] + '_' + name;
+      }
+      api.openFrame({
+        name: name,
+        url: url,
+        rect: {
+            x: 0,
+            y: 0,
+            w: 'auto',
+            h: 'auto'
+        },
+        pageParam: pageParams
+    });
+  },
+  open_win: function(url, pageParams = {}){
+    var arr = url.split('/');
+    var name = arr[arr.length - 1].split('.')[0];
+    if(!arr[arr.length - 2]) {
+      alert('url不规范');
+      return;
+    } else if(arr[arr.length - 2] && arr[arr.length - 2] == '.') {
+      var href = location.href.split('/');
+      name = href[href.length - 2] + '_' + name;
+    } else {
+      name = arr[arr.length - 2] + '_' + name;
+    }
+    api.openWin({
+      name: name,
+      url: url,
+      pageParam: pageParams
+  });
+  },
+  get: function(url, callback) {
+    var url_split = url.split(website);
+    if(!url_split[1]) {
+      url = website + url;
+    }
+    if($api.getStorage('token')) {
+      var token = $api.getStorage('token');
+      url = addParama(url, 'token', token);
+    }
+
+    url = addParama(url, 'device_code', '1');
+    $api.get(url, callback);
+  },
+  post: function(url, data, callback) {
+    var url_split = url.split(website);
+    if(!url_split[1]) {
+      url = website + url;
+    }
+
+    if($api.getStorage('token')) {
+      var token = $api.getStorage('token');
+      url = addParama(url, 'token', token);
+    }
+
+    url = addParama(url, 'device_code', '1');
+    $api.post(url, {values: data}, callback);
+  },
+
+  check_login: function(redirect) {
+    var self = this;
+    if($api.getStorage('token')) {
+        return true;
+    } else {
+      self.open_win({url:'../account/guide.html', pageParam: {'from': redirect}});
+      return false;
+    }
+  },
+  set_loginInfo: function(res) {
+    $api.setStorage('user_id', res.user_id);
+    $api.setStorage('head_pic', res.head_pic);
+    $api.setStorage('uname', res.uname);
+    $api.setStorage('mobile', res.mobile);
+    $api.setStorage('token', res.token);
+    var timestamp = Date.parse(new Date()) / 1000;
+    $api.setStorage('expire', timestamp + 2 * 30 * 24 * 3600 - 600);
+  },
+  echo: function(object) {
+    if(typeof(object) == "string") {
+      alert(object)
+    } else {
+      alert(JSON.stringify(object));
+      //jsonToStr
+    }
+  },
+  alert: function(msg, duration) {
+    if(!duration)
+      duration = 3000;
+    api.toast({
+        msg : msg,
+        duration : duration,
+        location : 'bottom'
+    });
+  },
+  clear: function() {
+    api.execScript({
+        name: 'root',
+        script: 'closeSideNav()'
+    });
+
+    api.toast({
+        msg : '清除成功',
+        duration : 2000,
+        location : 'bottom'
+    });
+  },
+  logout: function() {
+    var self = this;
+    $api.rmStorage('user_id');
+    $api.rmStorage('uname');
+    $api.rmStorage('head_pic');
+    $api.rmStorage('mobile');
+    $api.rmStorage('token');
+    /*
+    glo.get('/mobile/login/logout', function() {
+      self.clear();
+      api.closeWin();
+
+    });
+    */
+  },
+  reload: function(callback) {
+    var self = this;
+    api.setCustomRefreshHeaderInfo({
+      bgColor: '#fff',
+    }, function() {
+      api.refreshHeaderLoadDone();
+      callback();
+    });
+  },
+  scrolltobottom: function(callback){
+    var callback2 = function() {
+      api.toast({
+          msg : '加载更多...',
+          duration : 2000,
+          location : 'bottom'
+      });
+      callback();
+    }
+    api.addEventListener({
+      name:'scrolltobottom',
+      extra:{
+         threshold:0     //设置距离底部多少距离时触发，默认值为0，数字类型
+       }
+     }, callback2
+   );
+  },
+  show_progress: function() {
+    api.showProgress({
+     style: 'default',
+     animationType: 'zoom',
+     title: '',
+     text: '拼命的在加载...',
+     modal: true
+   });
+   setTimeout("glo.hide_progress()",3000);
+ },
+ hide_progress: function(){
+     api.hideProgress();
+ },
+
+
+}
+function checkMobile(tel) {
+    var reg = /(^1[3|4|5|7|8][0-9]{9}$)/;
+    if (reg.test(tel)) {
+        return true;
+    }else{
+        return false;
+    };
+}
+function addParama(url, key, val) {
+  if (url.indexOf('?') != -1) {
+    url += '&' + key + '=' + val;
+  } else {
+    url += '?' + key + '=' + val;
+  }
+  return url;
+}
