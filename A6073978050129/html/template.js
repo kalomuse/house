352a3860c48915475bf50c47a8dbf30b8531e724
kalomuse <jxz405101020@163.com>
2018-06-08 13:27:31 +0800

    templte

---
+++ A6073978050129/html/template.js
@@ -0,0 +1,22 @@
+//vue初始事例
+var vue = new Vue({
+  el: '.container',
+  data: {
+    website: website,
+  }
+})
+
+//点击事例，navigateTo对应open_win，redirectTo对应open_frame
+function bindViewsearch() {
+  glo.open_win('../search/search');
+}
+
+apiready = function() {
+  //初始化必须调用
+  glo.init();
+
+  //post事例
+  glo.post(website + '/api/region/setRegion', { regionid: vue.regionid }, function (res) {
+      vue.region =  res.data.area;
+  });
+};
