11409776b25f40d7206eb7612a557affaf15e05b
kalomuse <jxz405101020@163.com>
2018-06-09 14:37:16 +0800

    修改历史搜索

--- A6073978050129/html/real_search/real_search.js
+++ A6073978050129/html/real_search/real_search.js
@@ -6,7 +6,7 @@ var vue = new Vue({
     website: website,
     userInfo: {},
     hasUserInfo: false,
-    searchlist: {},
+    searchlist: [],
     hotwordlist:{}
   },
   methods:{
@@ -19,6 +19,14 @@ var vue = new Vue({
 //点击事例，navigateTo对应open_win，redirectTo对应open_frame
 function SearchHouse() {
   var keyword = $('#keyword').val();
+  var searchlist = vue.searchlist;
+  if(searchlist.length >= 3) {
+    searchlist.pop();
+    searchlist.unshift({keyword:keyword})
+  } else {
+    searchlist.unshift({keyword:keyword})
+  }
+  $api.setStorage('searchlist', searchlist);
   glo.open_win('../real_time/real_time', {keyword: keyword});
 }
 //楼盘事件处理函数
@@ -35,22 +43,24 @@ function bindViewnews() {
 }
 
 function bindClear_history() {
-  glo.post('/api/house/clearHistory', {}, function (res) {
+  $api.setStorage('searchlist', []);
+  vue.searchlist =  []
+  /*glo.post('/api/house/clearHistory', {}, function (res) {
       vue.searchlist = res.data;
       if (res.data.Code) {
-        vue.searchlist =  {}
+
       }
-  });
+  });*/
 }
 
 apiready = function() {
   //初始化必须调用
   glo.init();
-
+  vue.searchlist = $api.getStorage('searchlist')? $api.getStorage('searchlist'): [];
   //获取历史搜索
-  glo.post('/api/house/getHistory', {}, function (res) {
+  /*glo.post('/api/house/getHistory', {}, function (res) {
       vue.searchlist = res.data;
-  });
+  });*/
 
   //获取热门搜索
   glo.post('/api/house/getHotword', {}, function (res) {
