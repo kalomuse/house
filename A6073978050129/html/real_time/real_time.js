7ecd67f8ee1503cd051d5ace77b43e9c5d0687cd
kalomuse <jxz405101020@163.com>
2018-06-11 13:33:50 +0800

    add

--- A6073978050129/html/real_time/real_time.js
+++ A6073978050129/html/real_time/real_time.js
@@ -17,51 +17,51 @@ function realsearch() {
      });
      return false
    }
-   glo.show_progress('正在查询...');
+   setTimeout("glo.show_progress('正在查询...');", 300);
    $api.post('http://scrapy.jiashanquan.top/?a='+api.pageParam.keyword, {},function (res) {
-     glo.hide_progress();
-     var pricedata = JSON.parse(res.msg);
-          var index = res.msg.indexOf('{"report');
-          if (index == 1) {
-            index = 0;
-          } else {
-            index = 1;
+      glo.hide_progress();
+      var res = JSON.parse(res.msg), data = {}
+      for(index in res) {
+        for(key in res[index]) {
+          data[key] = res[index][key];
+          break;
+        }
+      }
+      var report = data.report;
+      if (report.length) {
+        var title = report[2] + report[3] + report[4] + report[5] + report[7];
+        $('.tit').html(title);
+        //获取数据
+        var biao = JSON.parse(data.biao);
+        var categories1 = biao[2].xAxis;
+        var pricedata1 = biao[0].series[0].data;
+        var y_mark = [];
+        var pricedata = [];
+        for (var i in pricedata1) {
+          if (pricedata1[i] != '-') {
+            pricedata.push(pricedata1[i]);
+            y_mark.push(categories1[i]);
           }
-          var report = pricedata[index].report;
-          if (report.length) {
-            var title = pricedata[index].report[2] + pricedata[index].report[3] + pricedata[index].report[4] + pricedata[index].report[5] + pricedata[index].report[7];
-            $('.tit').html(title);
-            //获取数据
-            var biao = JSON.parse(pricedata[index + 1].biao);
-            var categories1 = biao[2].xAxis;
-            var pricedata1 = biao[0].series[0].data;
-            var y_mark = [];
-            var pricedata = [];
-            for (var i in pricedata1) {
-              if (pricedata1[i] != '-') {
-                pricedata.push(pricedata1[i]);
-                y_mark.push(categories1[i]);
-              }
-            }
-            var ctx = document.getElementById("myChart").getContext("2d");
-            var myChart = new Chart(ctx, {
-              type: 'line',
-              data: {
-                 labels : y_mark,
-                 datasets : [
-                     {
-                         label: "最新房价",  //当前数据的说明
-                         fill: true,  //是否要显示数据部分阴影面积块  false:不显示
-                         borderColor: "rgba(75,192,192,1)",//数据曲线颜色
-                         pointBackgroundColor: "#fff", //数据点的颜色
-                         data: pricedata,  //填充的数据
-                     }
-                 ]
-              }
-            });
-          } else {
-            glo.alert('没有查到对应信息');
+        }
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+             labels : y_mark,
+             datasets : [
+                 {
+                     label: "最新房价",  //当前数据的说明
+                     fill: true,  //是否要显示数据部分阴影面积块  false:不显示
+                     borderColor: "rgba(75,192,192,1)",//数据曲线颜色
+                     pointBackgroundColor: "#fff", //数据点的颜色
+                     data: pricedata,  //填充的数据
+                 }
+             ]
           }
+        });
+      } else {
+        glo.alert('没有查到对应信息');
+      }
    });
 
 }
