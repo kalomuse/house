b246bb4014aac83b335646eef7ea947729c8c290
timo <1403716576@qq.com>
2018-06-15 15:43:25 +0800

    登陆

--- A6073978050129/html/login/login.js
+++ A6073978050129/html/login/login.js
@@ -1,10 +1,100 @@
 //vue初始事例
+var wait = 150;
 var vue = new Vue({
   el: '.container',
   data: {
     website: website,
   }
 })
+
+function sendvercode(o){
+
+  var phone = $.trim($('#phone').val());
+
+  if(phone==''){
+    glo.alert('手机号不能为空');
+    return false;
+  }
+  else if (!checkMobile(phone)) {
+    glo.alert('手机号不匹配');
+    return false;
+  }
+  else {
+      sendcode(o);
+  }
+
+}
+function sendcode(o){
+  var phone = $.trim($('#phone').val());
+  glo.post('/api/base/getCode',{
+    telephone:phone
+  },
+function(res){
+  if(res.Code==1){
+      countdown(o);
+  }else{
+    glo.alert("获取失败！")
+  }
+
+}
+)
+
+}
+
+
+function countdown(obj, msg) {
+
+    obj = $(obj);
+    if (wait == 0) {
+        obj.removeAttr("disabled");
+        obj.val(msg);
+        wait = 150;
+    } else {
+        if (msg == undefined || msg == null) {
+            msg = obj.val();
+        }
+        obj.attr("disabled", "disabled");
+        obj.val(wait + "秒后重新获取");
+        wait--;
+        setTimeout(function() {
+          countdown(obj, msg);
+        }, 1000)
+    }
+}
+
+
+function checkSub(){
+  var phone = $.trim($('#phone').val());
+  var verify_code = $.trim($('#dyn_pwd').val());
+  if(phone==''){
+    glo.alert('手机号不能为空');
+    return false;
+  }
+  else if (!checkMobile(phone)) {
+    glo.alert('手机号不匹配');
+    return false;
+  }
+else  if(verify_code==''){
+    glo.alert('验证码不能为空');
+    return false;
+  }
+  else{
+    glo.post('/api/user/appLogin',{
+      mobile:phone,
+      code:verify_code
+    },function(res){
+      if(res.Code==1){
+         $api.setStorage('token', res.data);
+      }else {
+        glo.alert("验证码有误")
+      }
+      // glo.echo(res);
+    }
+  );
+  }
+
+
+}
 
 //点击事例，navigateTo对应open_win，redirectTo对应open_frame
 function bindViewsearch() {
