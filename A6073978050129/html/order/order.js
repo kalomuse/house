72c35e58e314305d45b6c6178f7b5864cce68fa9
timo <1403716576@qq.com>
2018-06-16 16:01:22 +0800

    详细页面和预约看房

---
+++ A6073978050129/html/order/order.js
@@ -0,0 +1,53 @@
+var vue = new Vue({
+  el: '.container',
+  data: {
+    website: website,
+    houseid:'',
+    houseinfo:{},
+    housetype:{}
+  }
+});
+function submit(){
+  var id = api.pageParam.id;
+  var name= $.trim($('#name').val());
+  var telephone= $.trim($('#telephone').val());
+  var agent= $.trim($('agent').val());
+  var uid= $api.getStorage('token');
+if(!name){
+  glo.alert('请填写真实姓名');
+}
+else if (!telephone) {
+  glo.alert('请填写手机号');
+}
+else if (!checkMobile(telephone)) {
+  glo.alert('请输入正确手机号');
+}
+else {
+  glo.post('/api/customer/postCustomer',{
+    'uid': uid,
+          'name': name,
+          'telephone': telephone,
+          'house_id': id,
+          'agent': agent
+  },function(res){
+    if(res.Code){
+      glo.alert('预约看房成功！请等待客服经理电话');
+    setTimeout("api.closeWin()",2000);
+
+    }
+    else{
+      glo.alert('预约看房失败，请检查网络');
+    }
+    // glo.echo(res);
+  });
+}
+}
+
+apiready = function() {
+    glo.init();
+      var id = api.pageParam.id;
+      var title=api.pageParam.title;
+      // alert(title);
+      vue.houseinfo={title:title};
+
+}
--- A6073978050129/html/proview/proview.js
+++ A6073978050129/html/proview/proview.js
@@ -7,12 +7,26 @@ var vue = new Vue({
     housetype:{}
   }
 });
+function callMobile() {
+  api.call({
+      type: 'tel_prompt',
+      number: '10086'
+  });
+}
+function callWechat(){
+  alert('请加微信XXXXXXXXXXXXX')
+}
 
+function bindVieworder(){
+    var id = api.pageParam.id;
+  glo.open_win('../order/order',{id:id,title:vue.houseinfo.title});
+}
 apiready = function() {
   //初始化必须调用
   glo.init();
   //初始化数据
   var id = api.pageParam.id;
+  // alert(id);
   glo.post('/api/house/getHousedetail', { id: id }, function (res) {
       vue.houseinfo =  res.data;
       //glo.echo(res.data.img);
@@ -41,22 +55,21 @@ apiready = function() {
   });
   glo.post('/api/house/getHousetype', { id: id }, function (res) {
       vue.housetype =  res.data;
+      // glo.echo(res);
   });
-  function callMobile() {
 
-  }
-  callMobile:function(e){
-      var obj=this;
-      wx.makePhoneCall({
-        phoneNumber: obj.data.telephone, //此号码并非真实电话号码，仅用于测试
-      })
-    },
-    callWechat:function(e){
-      var obj = this;
-      wx.showToast({
-        title: '请添加微信号：' + obj.data.wechat,
-        icon: 'none',
-        duration: 5000
-      })
-    },
+  // callMobile:function(e){
+  //     var obj=this;
+  //     wx.makePhoneCall({
+  //       phoneNumber: obj.data.telephone, //此号码并非真实电话号码，仅用于测试
+  //     })
+  //   },
+  //   callWechat:function(e){
+  //     var obj = this;
+  //     wx.showToast({
+  //       title: '请添加微信号：' + obj.data.wechat,
+  //       icon: 'none',
+  //       duration: 5000
+  //     })
+  //   },
 };
