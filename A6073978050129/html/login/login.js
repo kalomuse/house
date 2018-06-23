02e2f0eafb3ee5bf080905d85ac08ebb4a0eb8df
kalomuse <jxz405101020@163.com>
2018-06-23 17:15:15 +0800

    c

--- A6073978050129/html/login/login.js
+++ A6073978050129/html/login/login.js
@@ -93,8 +93,6 @@ function authWX() {
 function getToken(code) {
   var wx = api.require('wx');
   wx.getToken({
-    apiKey: 'wx8519b390e429f7f8',
-    apiSecret: '2a4882ce70898b679f7904c66859d5808',
     code: code
   }, function(ret, err) {
     if (ret) {
